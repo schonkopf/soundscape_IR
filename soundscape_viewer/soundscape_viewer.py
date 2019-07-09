47b40d4331525c2d49b318c98fcd140438f43f09
Tzu-Hao Harry Lin <schonkopf@gmail.com>
2019-07-09 23:12:18 +0900

    Update soundscape_viewer.py

--- soundscape_viewer/soundscape_viewer.py
+++ soundscape_viewer/soundscape_viewer.py
@@ -13,8 +13,8 @@ from scipy.io import loadmat
 from scipy.io import savemat
 from sklearn.decomposition import PCA
 from sklearn.cluster import KMeans
-from .utility import save_parameters
-from .utility import gdrive_handle
+from soundscape_IR.soundscape_viewer.utility import save_parameters
+from soundscape_IR.soundscape_viewer.utility import gdrive_handle
 
 class lts_viewer:
   """
