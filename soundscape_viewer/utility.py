a824653efdc33c9608f2db681a7ee2d70ff654d8
Tzu-Hao Harry Lin <schonkopf@gmail.com>
2019-09-26 20:17:56 +0800

    Add matrix_operation

--- soundscape_viewer/utility.py
+++ soundscape_viewer/utility.py
@@ -56,3 +56,37 @@ class save_parameters:
     self.sensitivity=sensitivity 
     self.sampling_freq=sampling_freq 
     self.channel=channel
+
+class matrix_operation:
+  def __init__(self, header=[]):
+    self.header=header
+    
+  def gap_fill(self, time_vec, data):
+     # fill the gaps in a time series
+     temp = np.argsort(time_vec)
+     time_vec=time_vec[temp]
+     if data.ndim>1:
+         output=data[temp,:]
+     else:
+         output=data[temp]
+     resolution=np.round((time_vec[1]-time_vec[0])*24*3600)
+     n_time_vec=np.arange(np.floor(np.min(time_vec))*24*3600, 
+                             np.ceil(np.max(time_vec))*24*3600,resolution)/24/3600
+
+     if data.ndim>1:
+         save_result=np.zeros((n_time_vec.size, data.shape[1]+1))
+     else:
+         save_result=np.zeros((n_time_vec.size, 2))
+
+     save_result[:,0]=n_time_vec-693960
+     segment_list=np.round(np.diff(time_vec*24*3600)/resolution)
+     split_point=np.vstack((np.concatenate(([0],np.where(segment_list!=1)[0]+1)),
+                               np.concatenate((np.where(segment_list!=1)[0],[time_vec.size-1]))))
+
+     for run in np.arange(split_point.shape[1]):
+       i=np.argmin(np.abs(n_time_vec-time_vec[split_point[0,run]]))
+       if data.ndim>1:
+             save_result[np.arange(i,i+np.diff(split_point[:,run])+1),1:]=output[np.arange(split_point[0,run], split_point[1,run]+1),:]
+       else:
+             save_result[np.arange(i,i+np.diff(split_point[:,run])+1),1]=output[np.arange(split_point[0,run], split_point[1,run]+1)]
+     return save_result
