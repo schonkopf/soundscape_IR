ba7d4eccbc6df5265113941b6e12f2d4cb385785
Tzu-Hao Harry Lin <schonkopf@gmail.com>
2021-03-15 11:22:15 +0800

    Add functions to make LTS after source separation

--- soundscape_viewer/batch_processing.py
+++ soundscape_viewer/batch_processing.py
@@ -45,7 +45,7 @@ class batch_processing:
     print('Identified ', len(audioname), 'files')
     return Gdrive, link, audioname
 
-  def params_spectrogram(self, fft_size=512, environment='wav', time_resolution=None, window_overlap=0, f_range=[], prewhiten_percent=0, padding=0, folder_combine=False, mel_comp=0):
+  def params_spectrogram(self, fft_size=512, environment='wav', time_resolution=None, window_overlap=0, f_range=[], prewhiten_percent=0, padding=0, folder_combine=False, mel_comp=0, sensitivity=0):
     self.fft_size = fft_size 
     self.time_resolution = time_resolution
     self.window_overlap = window_overlap
@@ -58,6 +58,7 @@ class batch_processing:
     self.folder_combine=folder_combine
     self.annotation_padding=padding
     self.mel_comp=mel_comp
+    self.sensitivity=sensitivity
 
   def params_separation(self, model, iter=50, adaptive_alpha=0, additional_basis=0):
     self.model = model
@@ -98,13 +99,24 @@ class batch_processing:
     self.padding = padding
     self.folder_id = folder_id
     self.run_detection=True
+  
+  def lts_maker(self, source=1, time_resolution=[], dateformat='yyyymmdd_HHMMSS', initial=[], year_initial=2000, filename='Separation_LTS.mat', folder_id=[]):
+    self.run_lts=True
+    self.lts_time_resolution=time_resolution
+    self.dateformat=dateformat
+    self.initial=initial
+    self.year_initial=year_initial
+    self.lts_source=source
+    self.lts_filename=filename
+    self.lts_folder_id=folder_id
 
   def params_pulse_interval(self, energy_percentile=50, interval_range=[1, 1000]):
     self.energy_percentile=energy_percentile
     self.interval_range=interval_range
     self.run_pulse_analysis=True
   
-  def run(self, start=1):
+  def run(self, start=0, num_file=None):
+    from soundscape_IR.soundscape_viewer import lts_maker
     from soundscape_IR.soundscape_viewer.utility import audio_visualization
     from soundscape_IR.soundscape_viewer.utility import spectrogram_detection
     from soundscape_IR.soundscape_viewer.utility import pulse_interval
@@ -117,12 +129,18 @@ class batch_processing:
     if self.run_separation:
       model_backup = copy.deepcopy(self.model)
 
-    self.start = start-1
-    for file in range(self.start, len(self.audioname)):
+    self.start = start
+    if num_file:
+      run_list = range(self.start, self.start+num_file)
+    else:
+      run_list = range(self.start, len(self.audioname))
+
+    for file in run_list:
+      print('\r', end='')
       if self.cloud==1:
         urllib.request.urlretrieve(self.link[file], self.audioname[file])
         path='.'
-        print('Processing file no. '+str(file+1)+' :'+temp['title']+', in total: '+str(len(self.audioname))+' files')
+        print('Processing file no. '+str(file)+' :'+temp['title']+', in total: '+str(len(self.audioname))+' files', flush=True, end='')
       elif self.cloud==2:
         temp = self.Gdrive.file_list[file]
         temp.GetContentFile(temp['title'])
@@ -131,35 +149,48 @@ class batch_processing:
           temp2 = self.Gdrive_selections.file_list[0]
           temp2.GetContentFile(temp2['title'])
         path='.'
-        print('Processing file no. '+str(file+1)+' :'+temp['title']+', in total: '+str(len(self.audioname))+' files')
+        print('Processing file no. '+str(file)+' :'+temp['title']+', in total: '+str(len(self.audioname))+' files', flush=True, end='')
       else:
         temp = self.audioname[file]
         path = self.link
-        print('Processing file no. '+str(file+1)+' :'+temp+', in total: '+str(len(self.audioname))+' files')
+        print('Processing file no. '+str(file)+' :'+temp+', in total: '+str(len(self.audioname))+' files', flush=True, end='')
 
       
       if self.Raven_selections:
-        audio = audio_visualization(self.audioname[file], path, FFT_size = self.fft_size, time_resolution=self.time_resolution, window_overlap=self.window_overlap, f_range = self.f_range, 
+        audio = audio_visualization(self.audioname[file], path, FFT_size = self.fft_size, time_resolution=self.time_resolution, window_overlap=self.window_overlap, f_range = self.f_range, sensitivity=self.sensitivity, 
                                     environment=self.environment, plot_type=None, prewhiten_percent=self.prewhiten_percent, annotation = temp2['title'], padding = self.annotation_padding, mel_comp=self.mel_comp)
         if self.folder_combine:
           if file==0:
             folder_data=audio.data
-            time_notation=np.add((file+1)*np.ones((audio.data.shape[0],1),dtype = int)*10000, audio.time_notation)
+            time_notation=np.add((file)*np.ones((audio.data.shape[0],1),dtype = int)*10000, audio.time_notation)
           else:
             folder_data=np.vstack((folder_data, audio.data))
-            time_notation = np.vstack((time_notation, np.add((file+1)*np.ones((audio.data.shape[0],1),dtype = int)*10000, audio.time_notation)))
+            time_notation = np.vstack((time_notation, np.add((file)*np.ones((audio.data.shape[0],1),dtype = int)*10000, audio.time_notation)))
             
           folder_data[:,0]=np.arange(folder_data.shape[0])*(folder_data[1,0]-folder_data[0,0])
           self.spectrogram=np.array(folder_data)
           self.f=np.array(audio.f)
           self.time_notation=time_notation
       else:
-        audio = audio_visualization(self.audioname[file], path, FFT_size = self.fft_size, time_resolution=self.time_resolution, window_overlap=self.window_overlap, f_range = self.f_range,
+        audio = audio_visualization(self.audioname[file], path, FFT_size = self.fft_size, time_resolution=self.time_resolution, window_overlap=self.window_overlap, f_range = self.f_range, sensitivity=self.sensitivity,
                                   environment=self.environment, plot_type=None, prewhiten_percent=self.prewhiten_percent, mel_comp=self.mel_comp)
       
       if self.run_separation:
         model = copy.deepcopy(model_backup)
         model.supervised_separation(audio.data, audio.f, iter = self.iter, adaptive_alpha = self.adaptive_alpha, additional_basis = self.additional_basis)
+        if self.run_lts:
+          if file==self.start:
+            lts = lts_maker(time_resolution=self.lts_time_resolution)
+            lts.filename_check(self.dateformat, self.initial, self.year_initial, self.audioname[file])
+            lts.Result_median=np.array([])
+            lts.Result_mean=np.array([])
+            lts.f=audio.f
+            lts.sf=audio.sf
+            lts.link=[]
+          lts.get_file_time(self.audioname[file])
+          ambient=np.matlib.repmat(audio.ambient,model.separation[0].shape[0],1)
+          lts.Result_median, lts.Result_mean = lts.compress_spectrogram(10**((model.separation[self.lts_source-1][:,1:]+ambient)/10), model.separation[self.lts_source-1][:,0], lts.Result_median, lts.Result_mean, self.lts_time_resolution, linear_scale=True)
+
         if self.run_detection:
           for n in range(0, len(self.source)):
             filename=self.audioname[file][:-4]+'_S'+str(self.source[n])+'.txt'
@@ -185,3 +216,6 @@ class batch_processing:
         os.remove(self.audioname[file])
         if self.Raven_selections:
           os.remove(temp2['title'])
+
+    if self.run_lts:
+      lts.save_lts(self.lts_filename, self.lts_folder_id)
