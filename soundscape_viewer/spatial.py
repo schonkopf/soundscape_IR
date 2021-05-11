483e80ab6892df052e85cf2d911ea7c28f3e2ac6
Tzu-Hao Harry Lin <schonkopf@gmail.com>
2021-05-11 10:48:59 +0800

    Create spatial.py
    For analyzing spatial variation of soundscapes

---
+++ soundscape_viewer/spatial.py
@@ -0,0 +1,88 @@
+import pandas as pd
+import numpy as np
+from datetime import timedelta
+import matplotlib.pyplot as plt
+from scipy.interpolate import griddata
+
+class spatial_mapping():
+  def __init__(self, data, gps, gps_utc=0):
+    df=pd.DataFrame(data)
+    df[0]=pd.to_datetime(df[0]-693962,unit='D',origin=pd.Timestamp('1900-01-01'),utc=True)
+    df=df.rename(columns={0:'Time'})
+    self.data=df
+
+    if type(gps)==str:
+      if gps[-3:].lower()=='csv':
+        self.gps=pd.read_csv(gps)
+    self.gps.time=pd.to_datetime(self.gps.time)+timedelta(hours=gps_utc)    
+        
+  def extract_fragments(self, fragments, mean=True):
+    # fragments: a csv file contains beginning and ending time of recording sessions
+    # Or providing a time interval (seconds) for separating recording sessions
+    
+    if type(fragments)==str:
+      if fragments[-3:].lower()=='csv':
+        slice_df=pd.read_csv(fragments, sep=',')
+        slice_df['Begin_time']=pd.to_datetime(slice_df['Begin_time'],utc=True)
+        slice_df['End_time']=pd.to_datetime(slice_df['End_time'],utc=True)
+    elif type(fragments)==int:
+      segment_list=np.diff(data[:,0]*24*3600)
+      split_point=np.vstack((data[np.concatenate(([0],np.where(segment_list>fragments)[0]+1)),0],
+                       data[np.concatenate((np.where(segment_list>fragments)[0],[data.shape[0]-1])),0]))
+      slice_df = pd.DataFrame(split_point.T-693962, columns = ['Begin_time', 'End_time']) 
+      slice_df['Begin_time']=pd.to_datetime(slice_df['Begin_time'], unit='D',origin=pd.Timestamp('1900-01-01'),utc=True)
+      slice_df['End_time']=pd.to_datetime(slice_df['End_time'], unit='D',origin=pd.Timestamp('1900-01-01'),utc=True)
+      
+    ndf=pd.DataFrame()
+    for i in range(0, len(slice_df)):
+      temp=self.data['Time']-slice_df['Begin_time'][i]
+      data_list=np.where(temp>=timedelta(seconds=0))[0]
+      temp2=self.data['Time']-slice_df['End_time'][i]
+      data_list2=np.where(temp2<=timedelta(seconds=0))[0]
+      if mean:
+        result=np.mean(self.data.loc[data_list[temp[data_list].argmin()]:data_list2[temp2[data_list2].argmax()]+1])
+        result['Time']=self.data.loc[data_list[temp[data_list].argmin()],'Time']
+      else:
+        result=self.data.loc[data_list[temp[data_list].argmin()]:data_list2[temp2[data_list2].argmax()]+1]
+      ndf=ndf.append(result,ignore_index=True)
+    self.data=ndf
+            
+  def gps_mapping(self, tolerance=60):
+    # tolerance: seconds
+
+    for i in range(0,len(self.data)):
+      location=(self.gps['time']-self.data['Time'][i]).abs().argmin()
+      if (self.gps.loc[location, 'time']-self.data['Time'][i])>timedelta(seconds=tolerance):
+        self.data.loc[i, 'Latitude']=np.nan
+        self.data.loc[i, 'Longitude']=np.nan
+      else:
+        self.data.loc[i, 'Latitude']=self.gps.loc[location, 'lat']
+        self.data.loc[i, 'Longitude']=self.gps.loc[location, 'lon']
+        
+  def save_csv(self, filename='Spatial_mapping.csv'):
+    self.data.to_csv(filename, sep=',')
+    print('Successifully save to '+filename)
+    
+  def plot_map(self, plot_col=1, plot_type='contour', mapping_resolution=10, contour_levels=15, vmin=None, vmax=None):
+    # mapping resolution: meters
+
+    x=self.data['Longitude'][:]
+    y=self.data['Latitude'][:]
+    z=self.data[plot_col][:]
+    mapping_resolution=mapping_resolution/1000/1.852/60
+    xx=np.arange(np.floor(np.min(x)/mapping_resolution)*mapping_resolution,np.ceil(np.max(x)/mapping_resolution)*mapping_resolution+mapping_resolution,mapping_resolution)
+    yy=np.arange(np.floor(np.min(y)/mapping_resolution)*mapping_resolution,np.ceil(np.max(y)/mapping_resolution)*mapping_resolution+mapping_resolution,mapping_resolution)
+    grid_x, grid_y = np.meshgrid(xx, yy, indexing='ij')
+
+    plt.figure(figsize=(15, 12))
+    if plot_type=='contour' or plot_type=='both':
+      grid = griddata(np.hstack((x[:,None],y[:,None])), z, (grid_x, grid_y), method='cubic')
+      plt.contourf(grid.T, extent=(np.min(x),np.max(x),np.min(y),np.max(y)), levels=contour_levels, vmin=vmin, vmax=vmax)
+      cbar= plt.colorbar()
+    if plot_type=='scatter' or plot_type=='both':
+      plt.scatter(x, y, c=z, cmap='jet', vmin=vmin, vmax=vmax)
+      cbar= plt.colorbar()
+
+    self.grid=grid
+    self.x=xx
+    self.y=yy
