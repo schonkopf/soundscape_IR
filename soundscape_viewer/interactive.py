4936cc0fe2449e86e454ea1764de415fb6acfb7f
Tzu-Hao Harry Lin <schonkopf@gmail.com>
2021-05-12 13:05:38 +0800

    Update interactive.py

--- soundscape_viewer/interactive.py
+++ soundscape_viewer/interactive.py
@@ -6,7 +6,7 @@ Author: Tzu-Hao Harry Lin (schonkopf@gmail.com)
 import plotly.graph_objects as go
 import pandas as pd
 
-def interactive_matrix(input_data, f, vmin=None, vmax=None, x_title=None, y_title=None, x_date=True, figure_title=None, html_save=False, html_name='Interactive_matrix.html'):
+def interactive_matrix(input_data, f, vmin=None, vmax=None, x_title=None, y_title=None, x_date=True, figure_title=None, figure_plot=True, html_save=False, html_name='Interactive_matrix.html'):
   if x_date:
     fig = go.Figure(data=go.Heatmap(z=input_data[:,1:].T, 
         x=pd.to_datetime(input_data[:,0]-693962, unit='D',origin=pd.Timestamp('1900-01-01')), 
@@ -19,7 +19,8 @@ def interactive_matrix(input_data, f, vmin=None, vmax=None, x_title=None, y_titl
         colorscale='Jet', zmin = vmin, zmax = vmax))
 
   fig.update_layout(title=figure_title, yaxis_title=y_title, xaxis_title=x_title)
-  fig.show()
+  if figure_plot:
+    fig.show()
 
   if html_save:
     fig.write_html(file=html_name)
