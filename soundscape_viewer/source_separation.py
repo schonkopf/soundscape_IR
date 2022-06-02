ec9d1881cf50dfef6acd76bb46643e475e94ebce
Tzu-Hao Harry Lin <schonkopf@gmail.com>
2022-06-02 15:26:36 +0800

    Update source_separation.py

--- soundscape_viewer/source_separation.py
+++ soundscape_viewer/source_separation.py
@@ -340,7 +340,6 @@ class source_separation:
       W=np.array(self.W[:,W_list])
         
     # Plot
-    x_lim=[self.time_vec[H_list[0]][0], self.time_vec[H_list[-1]][0]]
     fig, ax = plt.subplots(figsize=(fig_width, fig_height))
     if plot_type=='W':
       im = ax.imshow(W[:,1:], origin='lower',  aspect='auto', cmap=cm.jet,
