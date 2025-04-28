86e8927f2489a0ce8f649b90f596cfc3c55cf03f
Tzu-Hao Harry Lin <schonkopf@gmail.com>
2025-04-28 15:48:40 +0800

    Update spatial.py

--- soundscape_viewer/spatial.py
+++ soundscape_viewer/spatial.py
@@ -169,7 +169,7 @@ class spatial_mapping():
     fig.update_layout(mapbox_style="carto-positron", mapbox_zoom=12,
         mapbox_center={"lat": np.mean(y), "lon": np.mean(x)},
         title=title)
-  fig.add_trace(go.Scattermapbox(lon=x, lat=y, text=t,
+    fig.add_trace(go.Scattermapbox(lon=x, lat=y, text=t,
         mode=plot_type, marker=dict(color=z, size=9, colorscale='Viridis', cmin=vmin, cmax=vmax, showscale=True),
         hovertemplate=
         "<b>%{text}</b><br><br>" +
